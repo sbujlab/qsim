857da5ba228640cf8ccd44b394c4294327db62f3
mcnudust <mcnulty@jlab.org>
2016-09-29 11:31:47 -0600

    Create qsimDetector.hh

---
+++ include/qsimDetector.hh
@@ -0,0 +1,39 @@
+#ifndef __QSIMDETECTOR_HH
+#define __QSIMDETECTOR_HH
+
+#include "G4VSensitiveDetector.hh"
+#include "qsimDetectorHit.hh"
+
+#include <map>
+
+/*! 
+      Default detector class.  This will record information on:
+
+      - Primary generated hit information
+
+*/
+
+class G4HCofThisEvent;
+class G4Step;
+class G4TouchableHistory;
+
+class qsimDetector : public G4VSensitiveDetector {
+    public:
+	qsimDetector( G4String name, G4int detnum );
+	virtual ~qsimDetector();
+
+	virtual void Initialize(G4HCofThisEvent*);
+	virtual G4bool ProcessHits(G4Step*,G4TouchableHistory*);
+	virtual void EndOfEvent(G4HCofThisEvent*);
+
+    private:
+	qsimDetectorHitsCollection *fHitColl;
+	G4int fHCID;
+
+	G4bool fTrackSecondaries;
+	G4int fDetNo;
+
+	G4double fDontRecordThresh;
+};
+
+#endif//__QSIMDETECTOR_HH
