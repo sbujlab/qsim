e784ae634b68dea39a833f5821f1ce3fbd41f2f1
audrey <audrey@mocha.physics.sunysb.edu>
2017-11-15 12:59:46 -0500

    Undefined reference errors

---
+++ build/CMakeFiles/qsimroot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qsimroot.dir/src/qsimRunData.cc.o"
+  "CMakeFiles/qsimroot.dir/src/qsimTextFile.cc.o"
+  "libqsimroot.pdb"
+  "libqsimroot.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qsimroot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
